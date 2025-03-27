0e869da0c279eab1ac7979cec891e525381065ca
Bibek Saini <itsbibeksaini@gmail.com>
2025-03-27 13:12:52 -0400

    fix: fixing test file

--- src/app/components/CoverPage/CoverPage.test.tsx
+++ src/app/components/CoverPage/CoverPage.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import CoverPage from './CoverPage';
 
 it('It should mount', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<CoverPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<CoverPage />);
+  root.unmount();
 });
