793680bf2179da1b1718f6ebc38f2b9adb61fad7
Bibek Saini <itsbibeksaini@gmail.com>
2025-03-27 11:43:36 -0400

    chore: static data files

--- src/app/data/portfolio-data.ts
+++ src/app/data/portfolio-data.ts
@@ -2,7 +2,7 @@ import IPortfolio from "./IPortfolio";
 
 const data:IPortfolio = {
     name: "Bibek Saini",
-    about: ["I specialise in designing architecture, developing seamless user experiences, and handling end-to-end deployment. Passionate about solving complex problems, I work across the front-end and back-end technologies—Java, .NET, Kotlin, Angular, React, Spring Boot, Docker, Kubernetes, and Azure, to name just a few. Whether it's a single line of clean code or performance optimisation, my ultimate aim is always to build trusted systems with real value."],
+    about: ["I specialise in designing architecture, developing seamless user experiences, and handling end-to-end deployment. Passionate about solving complex problems, I work across the front-end and back-end technologies — Java, .NET, Kotlin, Angular, React, Spring Boot, Docker, Kubernetes, and Azure, to name just a few. Whether it's a single line of clean code or performance optimisation, my ultimate aim is always to build trusted systems with real value."],
     role: "Senior Fullstack Software Engineer"
 }
 
