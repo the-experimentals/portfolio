e4dd259c63f54cd666525875a065d947c3bdfc11
Bibek Saini <itsbibeksaini@gmail.com>
2025-04-02 16:23:27 -0400

    feat: change atom state on click event

--- src/app/components/ContentPage/ContentPage.tsx
+++ src/app/components/ContentPage/ContentPage.tsx
@@ -1,29 +1,45 @@
+'use client';
+
 import React, { FC } from 'react';
 import { Grid, Typography } from '@mui/material';
 
 import profileImage from '../../assets/profile-image.jpeg';
 import SideBar from './SideBar/SideBar';
-import Image from 'next/image';
+import { useAtom } from 'jotai';
+import { sideBarAtom } from './SideBar/atoms/SideBatAtom';
 
-const ContentPage: FC = () => (
-  <Grid sx={{ height:'100%', width:'100%'}} container direction="column">
-    <Grid sx={{padding:'20px 30px'}} container gap={2}>
-        <Grid>
-            <Image src={profileImage.src} alt='profile image' style={{height:'70px', borderRadius:'50%', boxShadow:'5px 5px 10px var(--foreground)'}} />
+const ContentPage: FC = () => {
+  
+  const [sideBar] = useAtom(sideBarAtom);
+
+  return (
+    <Grid sx={{ height:'100%', width:'100%'}} container direction="column">
+      <Grid sx={{padding:'20px 30px'}} container gap={2}>
+          <Grid>
+              <img src={profileImage.src} alt='profile image' style={{height:'70px', borderRadius:'50%', boxShadow:'5px 5px 10px var(--foreground)'}} />
+          </Grid>
+          <Grid>
+            <Typography variant="h4" fontWeight={600}>
+              Bibek Saini
+            </Typography>
+            <Typography variant="h6" fontWeight={400}>
+              Senior Full Stack Software Engineer
+            </Typography>
+          </Grid>
+      </Grid>
+      <Grid size="grow" container>
+        <SideBar />
+        <Grid sx={{border:'1px solid red'}} size='grow'>
+          {
+            sideBar.workExperience && 
+            <Typography variant="h4" fontWeight={600} sx={{padding:'20px 30px'}}>
+              Work Experience
+              </Typography>
+          }
         </Grid>
-        <Grid>
-          <Typography variant="h4" fontWeight={600}>
-            Bibek Saini
-          </Typography>
-          <Typography variant="h6" fontWeight={400}>
-            Senior Full Stack Software Engineer
-          </Typography>
-        </Grid>
+      </Grid>
     </Grid>
-    <Grid size="grow" container>
-      <SideBar />
-    </Grid>
-  </Grid>
-);
+  )
+};
 
 export default ContentPage;
--- src/app/components/ContentPage/SideBar/SideBar.tsx
+++ src/app/components/ContentPage/SideBar/SideBar.tsx
@@ -3,62 +3,75 @@ import styles from './SideBar.module.scss';
 import { Grid, Typography } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase, faLaptopCode, faGears, faAddressBook, faFile } from '@fortawesome/free-solid-svg-icons';
+import { useAtom } from 'jotai';
+import { sideBarAtom } from './atoms/SideBatAtom';
 
-const SideBar: FC = () => (
-  <Grid className={`${styles.sidebar}`} >
-    <ul className="centralize">
-      <li className={`${styles.selected}`}>
+const SideBar: FC = () => {
+
+  const [sideBar, useSideBar] = useAtom(sideBarAtom);
+
+  function selectPage(){
+    useSideBar({
+      workExperience: false      
+    });
+  }
+
+  return (
+    <Grid className={`${styles.sidebar}`} >
+      <ul className="centralize">
+        <li className={`${styles.selected}`}>
+          <Grid container gap={4}>
+            <Grid sx={{position:'relative'}}>
+              <FontAwesomeIcon icon={faBriefcase} style={{fontSize:'2rem'}} className="centralize" />                
+            </Grid>
+            <Grid>
+              <Typography variant="h4">Work Experience</Typography>
+            </Grid>
+          </Grid>
+        </li>
+        <li onClick={selectPage}>
         <Grid container gap={4}>
-          <Grid sx={{position:'relative'}}>
-            <FontAwesomeIcon icon={faBriefcase} style={{fontSize:'2rem'}} className="centralize" />                
+            <Grid sx={{position:'relative'}}>
+              <FontAwesomeIcon icon={faLaptopCode} style={{fontSize:'2rem'}} className="centralize" />                
+            </Grid>
+            <Grid>
+              <Typography variant="h4">Projects</Typography>
+            </Grid>
           </Grid>
-          <Grid>
-            <Typography variant="h4">Work Experience</Typography>
+        </li>
+        <li>
+        <Grid container gap={4}>
+            <Grid sx={{position:'relative'}}>
+              <FontAwesomeIcon icon={faGears} style={{fontSize:'2rem'}} className="centralize" />                
+            </Grid>
+            <Grid>
+              <Typography variant="h4">Skills</Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </li>
-      <li>
-      <Grid container gap={4}>
-          <Grid sx={{position:'relative'}}>
-            <FontAwesomeIcon icon={faLaptopCode} style={{fontSize:'2rem'}} className="centralize" />                
+        </li>
+        <li>
+        <Grid container gap={4}>
+            <Grid sx={{position:'relative'}}>
+              <FontAwesomeIcon icon={faAddressBook} style={{fontSize:'2rem'}} className="centralize" />                
+            </Grid>
+            <Grid>
+              <Typography variant="h4">Contact</Typography>
+            </Grid>
           </Grid>
-          <Grid>
-            <Typography variant="h4">Projects</Typography>
+        </li>
+        <li>
+        <Grid container gap={4}>
+            <Grid sx={{position:'relative'}}>
+              <FontAwesomeIcon icon={faFile} style={{fontSize:'2rem'}} className="centralize" />                
+            </Grid>
+            <Grid>
+              <Typography variant="h4">Resume</Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </li>
-      <li>
-      <Grid container gap={4}>
-          <Grid sx={{position:'relative'}}>
-            <FontAwesomeIcon icon={faGears} style={{fontSize:'2rem'}} className="centralize" />                
-          </Grid>
-          <Grid>
-            <Typography variant="h4">Skills</Typography>
-          </Grid>
-        </Grid>
-      </li>
-      <li>
-      <Grid container gap={4}>
-          <Grid sx={{position:'relative'}}>
-            <FontAwesomeIcon icon={faAddressBook} style={{fontSize:'2rem'}} className="centralize" />                
-          </Grid>
-          <Grid>
-            <Typography variant="h4">Contact</Typography>
-          </Grid>
-        </Grid>
-      </li>
-      <li>
-      <Grid container gap={4}>
-          <Grid sx={{position:'relative'}}>
-            <FontAwesomeIcon icon={faFile} style={{fontSize:'2rem'}} className="centralize" />                
-          </Grid>
-          <Grid>
-            <Typography variant="h4">Resume</Typography>
-          </Grid>
-        </Grid>
-      </li>
-    </ul>
-  </Grid>
-);
+        </li>
+      </ul>
+    </Grid>
+  )
+};
 
 export default SideBar;
