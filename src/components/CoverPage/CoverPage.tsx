e4caa8bda3ee9ddb3db425ed43f9f04797b74596
Bibek Saini <itsbibeksaini@gmail.com>
2025-04-07 16:39:55 -0400

    cover page component

---
+++ src/components/CoverPage/CoverPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import styles from './CoverPage.module.scss';
+
+const CoverPage: FC = () => (
+  <div className={styles.CoverPage} data-testid="CoverPage">
+    CoverPage Component
+  </div>
+);
+
+export default CoverPage;
