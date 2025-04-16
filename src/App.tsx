12c36e2c24f44556336639671743e09f8fdbe0ef
Bibek Saini <itsbibeksaini@gmail.com>
2025-04-15 23:27:56 -0400

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import CoverPage from "./components/CoverPage/CoverPage"
 import styles from './App.module.scss'
 import WorkExperiencePage from "./components/WorkExperiencePage/WorkExperiencePage"
@@ -8,6 +8,14 @@ const App = () => {
     <Grid container className={`max-supported-width ${styles.portfolio}`}>
       <CoverPage/>
       <WorkExperiencePage/>
+      <Grid size={12} sx={{padding:'20px 30px'}}>
+        <Typography variant="subtitle2" component="h1" sx={{textAlign:'center'}}>
+          &copy; {new Date().getFullYear()}  - All rights reserved.
+        </Typography>
+        <Typography variant="subtitle2" component="h1" sx={{textAlign:'center'}}>
+          Designed with React and Material UI (MUI).
+        </Typography>
+      </Grid>
     </Grid>
   )
 }
