e8c9cc96124f705d51362a5bfd1d26b5f297eb6a
Bibek Saini <itsbibeksaini@gmail.com>
2025-04-30 00:02:25 -0400

    fix: typo fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Grid, Typography } from "@mui/material"
 import styles from './App.module.scss'
-import { WorkExperiencePage, CoverPage, ProjectsPage, SkillsPage, ContactsPage, MockupsGallary } from "./components"
+import { WorkExperiencePage, CoverPage, ProjectsPage, SkillsPage, ContactsPage, MockupsGallery } from "./components"
 
 const App = () => {
   return(
@@ -9,7 +9,7 @@ const App = () => {
       <WorkExperiencePage/>      
       <ProjectsPage/>
       <SkillsPage/>
-      <MockupsGallary/>
+      <MockupsGallery/>
       <ContactsPage/>
       <Grid size={12} sx={{padding:'20px 30px'}}>
         <Typography variant="subtitle2" component="h1" sx={{textAlign:'center'}}>
--- src/components/MockupsGallery/MockupsGallery.tsx
+++ src/components/MockupsGallery/MockupsGallery.tsx
@@ -1,26 +1,26 @@
 import { FC } from 'react';
-import styles from './MockupsGallary.module.scss';
+import styles from './MockupsGallery.module.scss';
 import { Grid, Typography, useTheme } from '@mui/material';
 
-export const MockupsGallary: FC = () => {
+export const MockupsGallery: FC = () => {
 
   const theme = useTheme();
 
   return (
-    <Grid size={12} container className={styles.MockupsGallary} data-testid="MockupsGallary" sx={{
+    <Grid size={12} container className={styles.MockupsGallery} data-testid="MockupsGallery" sx={{
       "--background-color": theme.palette.background.default,
     }}>
       <header>
         <Typography variant="h2" className={styles.title} fontWeight={400}>
-          Mockups Gallary
+          Mockups Gallery
         </Typography>
         <Typography variant="h6" className={styles.subTitle} fontWeight={400}>
           Sone of mockups on which I worked.
         </Typography>
       </header>
 
-      <Grid size={12} className={styles.gallaryFrameCover}>
-        <iframe src='https://mockups.itsbibeksaini.com/#/' className={`${styles.gallaryFrame}`} />
+      <Grid size={12} className={styles.galleryFrameCover}>
+        <iframe src='https://mockups.itsbibeksaini.com/#/' className={`${styles.galleryFrame}`} />
       </Grid>
     </Grid>
   )
